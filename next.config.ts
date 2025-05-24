b88b8e8db86a9986250b3f8d7c044251d718b7b5
Thalya Stéffany <studies.thalya@gmail.com>
2025-05-24 20:45:03 -0300

    chore(next.config): configure images url in remote patterns

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      new URL("https://i.postimg.cc/mgKz7XKb/credit-card-logo.png"), 
+      new URL("https://i.postimg.cc/s2KmjRvn/nfc.png"),
+      new URL ("https://i.postimg.cc/gcLh6jMX/logo-fox-prime-bank.png")
+    ]
+  }
 };
 
 export default nextConfig;
