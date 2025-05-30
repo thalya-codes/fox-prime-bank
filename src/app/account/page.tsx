cf5b5173320ffdc2bbae1687f7e0096448c6077f
Thalya Stéffany <studies.thalya@gmail.com>
2025-05-30 20:47:00 -0300

    refactor(account): move header and user greetings to layout

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,26 +1,21 @@
 'use client'
 import "../globals.css"
-import { Header } from "@/components/AccountPage/Header";
 import { CheckingAccountCard } from "@/components/AccountPage/CheckingAccountCard";
 import { NewTransaction } from "@/components/AccountPage/NewTransaction";
 import { BankStatements } from "@/components/AccountPage/BankStatements";
 
 export default function Home() {
     return (
-        <>
-           <Header />
-           <section className="h-full overflow-auto">
-                <h2 className="text-title text-brand-800 font-semibold text-6xl my-20">Olá, Jonh!</h2>
+        <section className="h-full overflow-auto">
 
-                <div className="flex justify-between flex-col gap-8 xl:flex-row">
-                    <div className="flex flex-col gap-12">
-                        <CheckingAccountCard />
-                        <NewTransaction />
-                    </div>
-
-                    <BankStatements />
+            <div className="flex justify-between flex-col gap-8 xl:flex-row">
+                <div className="flex flex-col gap-12">
+                    <CheckingAccountCard />
+                    <NewTransaction />
                 </div>
-           </section>
-        </>
+
+                <BankStatements />
+            </div>
+        </section>
     )
 }
