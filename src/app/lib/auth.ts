81c2999a5af4999d64646437176798dd1c621f97
Thalya Stéffany <studies.thalya@gmail.com>
2025-06-01 13:43:57 -0300

    feat(lib): create getDecodedAccessInfo util function

--- src/app/lib/auth.ts
+++ src/app/lib/auth.ts
@@ -1,5 +1,13 @@
 import jwt from 'jsonwebtoken';
+import {jwtDecode} from 'jwt-decode';
 import { TUser } from '../models/User';
+
+type TTokenPayload = {
+  userId: string;
+  iat: number;
+  exp: number;
+}
+
 const ACCESS_SECRET = process.env.ACCESS_SECRET!;
 const REFRESH_SECRET = process.env.REFRESH_SECRET!;
 
@@ -17,4 +25,15 @@ export function verifyAccessToken(token: string) {
 
 export function verifyRefreshToken(token: string) {
   return jwt.verify(token, REFRESH_SECRET) as Omit<TUser, '_id'> & {userId: string};
+}
+
+
+export function getDecodedAccessInfo(token: string): TTokenPayload | null {
+  try {
+    const decoded = jwtDecode<TTokenPayload>(token);
+    return decoded;
+  } catch (error) {
+    console.error('Erro ao decodificar token:', error);
+    return null;
+  }
 }
