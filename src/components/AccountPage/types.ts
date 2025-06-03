027d1cec61a8679932b1907eb8ea95be695088e0
Thalya Stéffany <studies.thalya@gmail.com>
2025-06-02 21:16:01 -0300

    feat: implement  api integration in private route header

--- src/components/AccountPage/types.ts
+++ src/components/AccountPage/types.ts
@@ -1,7 +1,8 @@
 import {  Dispatch, SetStateAction } from "react"
-export type TTransactionTypeFieldValues = 0 | 1 | 2
+export type TTransactionTypeFieldValues = 'TRANSFER'
 
 export type TTransactionTypeField = {
     defaultValue?: TTransactionTypeFieldValues
     onChange?: Dispatch<SetStateAction<TTransactionTypeFieldValues | undefined>> 
+    disabled?: boolean
 }
