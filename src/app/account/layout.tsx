85ee4ef6ab8398117a244ecd2c379f6b81437611
Thalya Stéffany <studies.thalya@gmail.com>
2025-06-02 21:23:54 -0300

    feat: wrapper AccountLayout with ProtectedRoute

--- src/app/account/layout.tsx
+++ src/app/account/layout.tsx
@@ -1,12 +1,17 @@
+'use client'
 import { Header } from "@/components/AccountPage/Header";
+import { ProtectedRoute } from "@/components/ProtectedRoute";
 import { ReactNode } from "react";
+import { useAccountContext } from "../context/AccountContext";
 
 export default function AccountLayout({ children }: { children: ReactNode }) {
+    const { userName } = useAccountContext()
+
     return (
-        <>
+        <ProtectedRoute>
             <Header />
-            <h2 className="text-title text-brand-800 font-semibold text-6xl my-20">Olá, Jonh!</h2>
+            <h2 className="text-title text-brand-800 font-semibold text-6xl my-20">Olá, {userName}!</h2>
             {children}
-        </>
+        </ProtectedRoute>
     )
 }
