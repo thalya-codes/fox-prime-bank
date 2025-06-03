21a0e90de98a08ff4f92b6b600b1cb98af0b8705
Thalya Stéffany <studies.thalya@gmail.com>
2025-06-02 21:01:09 -0300

    feat: pass user fullname to generateAccessToken function

--- src/app/lib/auth.ts
+++ src/app/lib/auth.ts
@@ -2,8 +2,9 @@ import jwt from 'jsonwebtoken';
 import {jwtDecode} from 'jwt-decode';
 import { TUser } from '../models/User';
 
-type TTokenPayload = {
+export type TTokenPayload = {
   userId: string;
+  fullName: string;
   iat: number;
   exp: number;
 }
@@ -11,8 +12,8 @@ type TTokenPayload = {
 const ACCESS_SECRET = process.env.ACCESS_SECRET!;
 const REFRESH_SECRET = process.env.REFRESH_SECRET!;
 
-export function generateAccessToken(userId: string) {
-  return jwt.sign({ userId }, ACCESS_SECRET, { expiresIn: '20m' });
+export function generateAccessToken(userId: string, fullName: string) {
+  return jwt.sign({ userId, fullName }, ACCESS_SECRET, { expiresIn: '20m' });
 }
 
 export function generateRefreshToken(userId: string) {
@@ -35,5 +36,31 @@ export function getDecodedAccessInfo(token: string): TTokenPayload | null {
   } catch (error) {
     console.error('Erro ao decodificar token:', error);
     return null;
+  }
+}
+
+export function verifyIfTokenIsExpired(token: string) {
+  if (!token) return true;
+
+  const payload = JSON.parse(atob(token.split('.')[1]));
+  const currentTime = Math.floor(Date.now() / 1000); // em segundos
+
+  return payload.exp < currentTime;
+}
+
+export function getTokenExpiration(token: string): number {
+  const payload = JSON.parse(atob(token.split('.')[1]));
+  return payload.exp * 1000; 
+}
+
+export function scheduleTokenRefresh(token: string, refresh: () => Promise<void>) {
+  const expiration = getTokenExpiration(token);
+  const now = Date.now();
+  const timeUntilExpiration = expiration - now;
+
+  const refreshTime = timeUntilExpiration - 60_000;
+
+  if (refreshTime > 0) {
+    setTimeout(refresh, refreshTime);
   }
 }
