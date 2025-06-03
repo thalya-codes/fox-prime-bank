153c7976f45876c971fab4f84b75589422da3ddd
Thalya Stéffany <studies.thalya@gmail.com>
2025-06-02 22:54:46 -0300

    fix: remove uneccessary conditional of AccountProvider

--- src/app/providers/AccountProvider.tsx
+++ src/app/providers/AccountProvider.tsx
@@ -4,22 +4,17 @@ import { getUserName } from "../lib/user";
 import { getUserAccount } from "../services/account";
 import { ReactNode } from "react";
 import { getAllUserTransactions } from "../services/transactions";
-import { usePathname } from "next/navigation";
 
 export  function AccountProvider({ children }: { children: ReactNode }) {
-    const isProtectedPage = usePathname().startsWith('/account')
-
     const userName = getUserName()
     const userAccount= useQuery({
         queryKey: ['user-account'],
         queryFn: getUserAccount,
-        enabled: isProtectedPage
     });
 
     const transactions = useQuery({
         queryKey: ['user-transactions'],
         queryFn: getAllUserTransactions,
-        enabled: isProtectedPage
     });
 
     return (
