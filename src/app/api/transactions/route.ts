4cdce498f260021b5e674c9e9424e5d1696f2b8f
Thalya Stéffany <studies.thalya@gmail.com>
2025-06-02 21:12:13 -0300

    refactor: remove comments in transactions

--- src/app/api/transactions/route.ts
+++ src/app/api/transactions/route.ts
@@ -10,14 +10,11 @@ export async function POST(req: NextRequest) {
   const authResponse = await authMiddleware(req) as JwtPayload
   if(authResponse instanceof NextResponse) return authResponse
 
-  //Extrai infos 
   const { fromAccountNumber, toAccountNumber, amount, description, nature } = await req.json();
 
-  //Verifica busca usuário responsável pelo envio e verifica se ele está cadastrado
   const userSender = await User.findById((authResponse).userId);
   if (!userSender) return NextResponse.json({ message: 'User not found' }, { status: 404 });
 
-  // Busca a conta de origem
   const fromAccount = await Account.findOne({ accountNumber: fromAccountNumber });
 
  if (!fromAccount) {
@@ -28,35 +25,29 @@ export async function POST(req: NextRequest) {
 
 
   
-  // Verifica se a conta pertence ao usuário autenticado
   if (String(fromAccount.userId) !== String(userSender?._id)) {
     return NextResponse.json({ message: 'Você não tem permissão para realizar esta operação.' }, { status: 403 });
   }
 
-  // Busca a conta de destino
   const toAccount = await Account.findOne({ accountNumber: toAccountNumber });
 
   const receiverUser = await User.findById(toAccount.userId);
-  // return NextResponse.json(toAccount)
   if (!receiverUser) return NextResponse.json({ message: 'Recipient user not found' }, { status: 404 });
 
   if (!toAccount) {
     return NextResponse.json({ message: 'Conta de destino não encontrada.' }, { status: 404 });
   }
 
-  // Verifica saldo suficiente
   if (fromAccount.balance < amount) {
     return NextResponse.json({ message: 'Saldo insuficiente.' }, { status: 422 });
   }
 
-  // Realiza transferência
   fromAccount.balance -= amount;
   toAccount.balance += amount;
 
   await fromAccount.save();
   await toAccount.save();
 
-  // Registra a transação
   const outgoingTransaction = await Transaction.create({
     amount,
     description,
@@ -69,7 +60,7 @@ export async function POST(req: NextRequest) {
   });
 
   const incomingTransaction = await Transaction.create({
-    amount,
+    amount: Number(amount),
     description,
     nature,
     senderId: userSender._id,
