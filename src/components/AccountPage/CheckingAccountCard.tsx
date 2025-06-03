d66c31d2247380d785f08e236a1a4f5d99ad4e0f
Thalya Stéffany <studies.thalya@gmail.com>
2025-06-02 21:14:16 -0300

    feat: implement CheckingAccountCard api integration

--- src/components/AccountPage/CheckingAccountCard.tsx
+++ src/components/AccountPage/CheckingAccountCard.tsx
@@ -1,8 +1,21 @@
 import Image from "next/image";
 import EyeOffIcon from "@/assets/eye-off.svg";
 import { CreditCard } from "fox-neo-design-system/dist/components/CreditCard/compositions";
+import { useAccountContext } from "@/app/context/AccountContext";
+import { useState } from "react";
 
 export function CheckingAccountCard() {
+    const {userAccount} = useAccountContext()
+    const [showBalance, setShowBalance] = useState<boolean>(JSON.parse(localStorage.getItem('show-balance') || '') || false)
+    
+    const toggleBalanceVisibility = () => {
+        setShowBalance((prev) => {
+            const newValue = !prev;
+            localStorage.setItem('show-balance', JSON.stringify(newValue));
+            return newValue;
+        });
+    };
+
     return (
         <CreditCard.Root className="flex flex-col gap-5 w-full xl:w-[471.75px]">
             <p className="font-title font-medium text-[32px] text-brand-950">Conta corrente</p>
@@ -12,10 +25,10 @@ export function CheckingAccountCard() {
             <div className="flex justify-between items-end">
                 <div className="flex flex-col gap-2">
                     <p className="font-title font-medium text-[28px] text-brand-950">Saldo</p>
-                    <p className="font-body font-medium text-2xl text-brand-950">R$ 30.000</p>
+                    <p className="font-body font-medium text-2xl text-brand-950"> {userAccount?.isLoading ?  'Carregando...' :  showBalance === true ? userAccount?.data?.balance : '***'}</p>
                 </div>
 
-                <button>
+                <button onClick={toggleBalanceVisibility}>
                     <Image src={EyeOffIcon} width={48} height={48} alt="Ocultar saldo"/>
                 </button>
             </div>
