43540e75b4ae86ffd7623cc471793da061472c74
Thalya Stéffany <studies.thalya@gmail.com>
2025-06-02 21:08:50 -0300

    feat: add treatment to remove access token when it expired

--- src/app/lib/localStorage.ts
+++ src/app/lib/localStorage.ts
@@ -1,7 +1,15 @@
+import { verifyIfTokenIsExpired } from "./auth"
+
 export function verifyIfUserHasAccessToken() {
     const accessToken = localStorage.getItem('access_token')
 
     if(!accessToken) return null
+    const isTokenExpired= verifyIfTokenIsExpired(accessToken)
+
+    if(isTokenExpired) {
+        localStorage.removeItem('access_token')
+        return null
+    }
     
     return accessToken
 }
